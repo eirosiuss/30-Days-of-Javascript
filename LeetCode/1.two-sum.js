e2bc97fbec897a67baa0ec15a9dad3e740258fbd
Kestutis Eirosius <eirosiuss@gmail.com>
2025-10-09 14:09:46 +0300

    two-sum map

--- LeetCode/1.two-sum.js
+++ LeetCode/1.two-sum.js
@@ -10,14 +10,34 @@
  * @param {number} target
  * @return {number[]}
  */
+
+// var twoSum = function (nums, target) {
+
+//   for (let i = 0; i < nums.length; i++) {
+//     // console.log('i'+i);
+
+//     for (let j = i + 1; j < nums.length; j++) {
+//       // console.log(nums[i],nums[j],'=', (nums[i]+nums[j]));
+
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
+
 var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i,j]
-      }
+  const map = new Map();
+  for (let index = 0; index < nums.length; index++) {
+    map.set(nums[index], index);
+  }
+  for (let index = 0; index < nums.length; index++) {
+    const complement = target - nums[index];
+    if (map.has(complement) && map.get(complement) !== index) {
+      return [index, map.get(complement)];
     }
   }
+  return []
 };
 
 // @lc code=end
