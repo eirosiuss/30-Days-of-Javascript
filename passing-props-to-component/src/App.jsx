042a3006aaae68a03d8388888963279f8269e713
Kestutis Eirosius <eirosiuss@gmail.com>
2025-10-30 17:00:12 +0200

    add initial info

--- passing-props-to-component/src/App.jsx
+++ passing-props-to-component/src/App.jsx
@@ -1,30 +1,31 @@
-import ProfileThree from './ProfileThree.jsx'
-import ProfileFour from './ProfileFour.jsx'
-import PackingList from './PackingList.jsx'
-import PackingListTwo from './PackingListTwo.jsx'
-import DrinkList from './DrinkList.jsx'
-import ScientistsList from './ScientistsList.jsx'
-import RecipeList from './RecipeList.jsx'
-import RecipeListTwo from './RecipeListTwo.jsx'
-import Poem from './Poem.jsx'
-import GalleryTwo from './GalleryTwo.jsx'
+import ProfileThree from "./ProfileThree.jsx";
+import ProfileFour from "./ProfileFour.jsx";
+import PackingList from "./PackingList.jsx";
+import PackingListTwo from "./PackingListTwo.jsx";
+import DrinkList from "./DrinkList.jsx";
+import ScientistsList from "./ScientistsList.jsx";
+import RecipeList from "./RecipeList.jsx";
+import RecipeListTwo from "./RecipeListTwo.jsx";
+import Poem from "./Poem.jsx";
+import GalleryTwo from "./GalleryTwo.jsx";
 import "./App.css";
-import Form from './Form.jsx'
-import FeedbackForm from './FeedbackForm.jsx'
-import FeedbackFormTwo from './FeedbackFormTwo.jsx'
-import RequestTracker from './RequestTracker.jsx'
-import Scoreboard from './Scoreboard.jsx'
-import Canvas from './Canvas.jsx'
-import ShoppingCart from './ShoppingCart.jsx'
-import ShoppingCartTwo from './ShoppingCartTwo.jsx'
-import TaskApp from './TaskApp.jsx'
-import Picture from './Picture.jsx'
-import EditProfile from './EditProfile.jsx'
-import MailClient from './MailClient.jsx'
-import SyncedInputs from './SyncedInputs.jsx'
-import FilterableList from './FilterableList.jsx'
-import FormTwo from './FormTwo.jsx'
-import FormThree from './FormThree.jsx'
+import Form from "./Form.jsx";
+import FeedbackForm from "./FeedbackForm.jsx";
+import FeedbackFormTwo from "./FeedbackFormTwo.jsx";
+import RequestTracker from "./RequestTracker.jsx";
+import Scoreboard from "./Scoreboard.jsx";
+import Canvas from "./Canvas.jsx";
+import ShoppingCart from "./ShoppingCart.jsx";
+import ShoppingCartTwo from "./ShoppingCartTwo.jsx";
+import TaskApp from "./TaskApp.jsx";
+import Picture from "./Picture.jsx";
+import EditProfile from "./EditProfile.jsx";
+import MailClient from "./MailClient.jsx";
+import SyncedInputs from "./SyncedInputs.jsx";
+import FilterableList from "./FilterableList.jsx";
+import FormTwo from "./FormTwo.jsx";
+import FormThree from "./FormThree.jsx";
+import ContactManager from "./ContactManager.jsx";
 function getImageUrl(imageId, size = "s") {
   return "https://i.imgur.com/" + imageId + size + ".jpg";
 }
@@ -104,8 +105,7 @@ function Gallery() {
         profession="geochemist"
         awards={["Miyake Prize for geochemistry", "Tanaka Prize"]}
         discovered="a method for measuring carbon dioxide in seawater"
-      >
-      </ProfileTwo>
+      ></ProfileTwo>
     </div>
   );
 }
@@ -113,36 +113,63 @@ function Gallery() {
 function App() {
   return (
     <>
-    <FormThree/>
-    <FormTwo/>
-    <FilterableList/>
-    <SyncedInputs/>
-    <MailClient/>
-    <EditProfile/>
-    <Picture/>
-    <TaskApp/>
-    <ShoppingCartTwo/>
-    <ShoppingCart/>
-    <Canvas/>
-    <Scoreboard/>
-    <RequestTracker/>
-    <FeedbackFormTwo/>
-    <FeedbackForm/>
-    <Form/>
-    <GalleryTwo/>
-    <Poem/>
-    <RecipeListTwo/>
-    <Gallery />
-    <ProfileThree/>
-    <ProfileFour/>
-    <PackingList/>
-    <PackingListTwo/>
-    <DrinkList/>
-    <ScientistsList/>
-    <RecipeList/>
+      <ContactManager />
+      <hr />
+      <FormThree />
+      <hr />
+      <FormTwo />
+      <hr />
+      <FilterableList />
+      <hr />
+      <SyncedInputs />
+      <hr />
+      <MailClient />
+      <hr />
+      <EditProfile />
+      <hr />
+      <Picture />
+      <hr />
+      <TaskApp />
+      <hr />
+      <ShoppingCartTwo />
+      <hr />
+      <ShoppingCart />
+      <hr />
+      <Canvas />
+      <hr />
+      <Scoreboard />
+      <hr />
+      <RequestTracker />
+      <hr />
+      <FeedbackFormTwo />
+      <hr />
+      <FeedbackForm />
+      <hr />
+      <Form />
+      <hr />
+      <GalleryTwo />
+      <hr />
+      <Poem />
+      <hr />
+      <RecipeListTwo />
+      <hr />
+      <Gallery />
+      <hr />
+      <ProfileThree />
+      <hr />
+      <ProfileFour />
+      <hr />
+      <PackingList />
+      <hr />
+      <PackingListTwo />
+      <hr />
+      <DrinkList />
+      <hr />
+      <ScientistsList />
+      <hr />
+      <RecipeList />
     </>
-
-  ) 
+  );
 }
 
 export default App;
