549d20e2322f8656bdce68d7094987304d5de2bf
Kestutis Eirosius <eirosiuss@gmail.com>
2025-11-18 17:41:09 +0200

    Replace prop drilling with context  finished

--- passing-props-to-component/src/ReplaceProp.jsx
+++ passing-props-to-component/src/ReplaceProp.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import { getImageUrl, places } from './utils.js';
+import { useState, useContext } from "react";
+import { SizeContext } from "./context.js";
+import { getImageUrl, places } from "./utils.js";
 
 export default function ReplaceProp() {
   const [isLarge, setIsLarge] = useState(false);
@@ -10,46 +11,43 @@ export default function ReplaceProp() {
         <input
           type="checkbox"
           checked={isLarge}
-          onChange={e => {
+          onChange={(e) => {
             setIsLarge(e.target.checked);
           }}
         />
         Use large images
       </label>
       <hr />
-      <List imageSize={imageSize} />
+      <SizeContext value={imageSize}>
+        <List />
+      </SizeContext>
     </>
-  )
+  );
 }
 
-function List({ imageSize }) {
-  const listItems = places.map(place =>
+function List({}) {
+  const listItems = places.map((place) => (
     <li key={place.id}>
-      <Place
-        place={place}
-        imageSize={imageSize}
-      />
+      <Place place={place} />
     </li>
-  );
+  ));
   return <ul>{listItems}</ul>;
 }
 
-function Place({ place, imageSize }) {
+function Place({ place }) {
   return (
     <>
-      <PlaceImage
-        place={place}
-        imageSize={imageSize}
-      />
+      <PlaceImage place={place} />
       <p>
         <b>{place.name}</b>
-        {': ' + place.description}
+        {": " + place.description}
       </p>
     </>
   );
 }
 
-function PlaceImage({ place, imageSize }) {
+function PlaceImage({ place }) {
+  const imageSize = useContext(SizeContext);
   return (
     <img
       src={getImageUrl(place)}
